04a428f2e27c2789238d976cdc09119e9bc125e1
Chen Ye <dpp0921@gmail.com>
2018-01-11 03:20:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,9 @@
-const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
-  let index = 0;
-  const key = parseInt(e.which);
-  if(key === alphabet[index]){
-    index++;
-    if(index === alphabet.length){
-      alert("Hurray!");
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
+  const script = document.querySelector('script');
+  input.addEventListener('keydown', function(e)){
+    console.log(e.which)
+  })
 }
