ed284f43319ec3408e0b9e1f0c44dd24e5470fdd
Chen Ye <dpp0921@gmail.com>
2018-01-11 03:30:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,16 +3,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   var i = 0;
-  var e = 0;
-  const key = parseInt(e.which);
-  if(key === code[i]){
-    i++;
-    if(i === code.length){
-      alert("Hurray!");
+  document.body.addEventListener('keydown', function(2)){
+    const key = parseInt(e.detail || e.which);
+    if(key === code[i]){
+      i++;
+      if(i === code.length){
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
-  return i;
+})
 }
